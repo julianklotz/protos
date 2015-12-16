a6e6df88962258ad19de62666147ebb56f0fd4c2
Julian Klotz <julian.klotz@outdooractive.com>
2015-12-16 15:45:55 +0100

    Jimmy

--- js/protos.js
+++ js/protos.js
@@ -7,11 +7,31 @@ var PROTOS = PROTOS || {};
 PROTOS.modal = (function() {
 	var exports = {};
 
+	exports.SIZES = {
+		S: 'protos-overlay-small',
+		M: 'protos-overlay-medium',
+		L: 'protos-overlay-large'
+	};
+
+	exports.SIZE_DEFAULT = exports.SIZES.M;
+
 	var contentElement = $('#protos-overlay-content');
 	var overlay = $('#protos-overlay');
 	var btnClose = $('.js-protos-overlay-close');
+	var inner = $('.protos-overlay-inner');
+	var currentSize = null;
 
-	exports.show = function(templateName) {
+	exports.show = function(templateName, newSize) {
+		if( !newSize ) {
+				newSize = exports.SIZE_DEFAULT;
+		}
+
+		if( currentSize != newSize ) {
+				inner.removeClass(currentSize);
+				inner.addClass(newSize);
+				currentSize = newSize;
+		}
+
 		modalContents = $('#' + templateName).html();
 		contentElement.html(modalContents);
 		overlay.show();
