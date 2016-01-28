ef0fbf834197845c5627417efb9a3b8af92e27c0
Julian Klotz <julian.klotz@outdooractive.com>
2016-01-28 15:15:02 +0100

    In place editing view, image upload

--- js/protos.js
+++ js/protos.js
@@ -30,6 +30,60 @@ PROTOS.StateMachine = Backbone.Model.extend({
 	},
 });
 
+PROTOS.InPlaceEditingView = Backbone.View.extend({
+	textSelector: '.js-text-input',
+	events: {
+		'click .js-btn-add-text': 'didPressAddText',
+		'click .js-btn-edit-text': 'didPressEditText',
+		'click .js-btn-save-text': 'didPressSaveText',
+		'click .js-btn-abort-text': 'didPressAbortText',
+	},
+
+	initialize: function() {
+		this.textValue = this.model.get('name');
+		this.stateMachine = new PROTOS.StateMachine(['SET', 'EDITING']);
+		this.stateMachine.on('change', this.render, this);
+		_.bindAll(this, 'didPressAddText', 'didPressEditText', 'didPressSaveText', 'didPressAbortText');
+	},
+
+	didPressAddText: function() {
+		this.stateMachine.setState('EDITING');
+		return false
+	},
+
+	didPressEditText: function() {
+		this.stateMachine.setState('EDITING');
+		return false
+	},
+
+	didPressSaveText: function() {
+		this.model.set('name', this.$(this.textSelector).val() );
+		this.leaveEditingMode();
+		return false
+	},
+
+	didPressAbortText: function() {
+		this.leaveEditingMode();
+		return false
+	},
+
+	leaveEditingMode: function() {
+		this.stateMachine.setState('SET');
+	},
+
+	render: function() {
+		console.log('state: ', this.stateMachine.getState() );
+		var template = _.template( $('#tpl-edit-title').html() );
+
+		var args = {
+			state: this.stateMachine.getState(),
+			textValue: this.model.get('name'),
+		};
+		this.$el.html( template(args) );
+		return this
+	},
+});
+
 PROTOS.modal = (function() {
 	"use strict";
 
@@ -48,6 +102,7 @@ PROTOS.modal = (function() {
 	var btnClose = $('.js-protos-overlay-close');
 	var inner = $('.protos-overlay-inner');
 	var currentSize = null;
+	var bodyClass = 'protos-overlay-open';
 
 	function setSize(newSize) {
 		if( !newSize ) { newSize = exports.SIZE_DEFAULT; }
@@ -59,25 +114,48 @@ PROTOS.modal = (function() {
 		}
 	}
 
+	var show = function() {
+		overlay[0].style.top = window.scrollY + 'px';
+		overlay.show();
+		$('body').addClass( bodyClass );
+	};
+
 	exports.render = function(templateName, newSize, tplParams) {
 		setSize(newSize);
 		var tpl = _.template( $('#' + templateName).html() );
 		contentElement.html( tpl( tplParams ) );
-		overlay.show();
+		show();
 	};
 
 	exports.showHtmlOnly = function(html, newSize) {
 		setSize(newSize);
 		contentElement.html( html );
-		overlay.show();
+		show();
 	}
 
 	exports.hide = function() {
 		contentElement.empty();
 		overlay.hide();
+		$('body').removeClass( bodyClass );
 	};
 
 	btnClose.on('click', exports.hide);
 
 	return exports
 })();
+
+PROTOS.formUtils = {};
+PROTOS.formUtils.getImagePreview = function(input, callback, context) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			if(!context) {
+				context = window;
+			}
+			callback.call(context, e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
